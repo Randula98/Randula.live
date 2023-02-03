313332d34b80887fa3e194e4a26630e27a844a21
Randula Mawaththta <randula98@gmail.com>
2023-02-03 10:28:14 +0530

    Hero Section Implemented

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,13 @@
 import Hero  from "./Hero";
+import About from "./About";
+import Projects from "./Projects";
+import Tools from "./Tools";
+import Contact from "./Contact";
 
-export {Hero};
+export {
+    Hero,
+    About,
+    Contact,
+    Tools,
+    Projects
+};
